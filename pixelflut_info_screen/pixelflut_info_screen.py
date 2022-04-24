30760111347cd5396f6171d6a541da6c7093b45e
HACKER3000 <hacker3000@posteo.org>
2022-04-24 15:03:11 +0200

    fixed small bug
    python 2 != python3

--- pixelflut_info_screen/pixelflut_info_screen.py
+++ pixelflut_info_screen/pixelflut_info_screen.py
@@ -8,12 +8,13 @@ pxf = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 pxf.connect(('localhost',1337))
 
 def send_pixel(x,y,r,g,b):
-    pxf.send('PX %d %d %02x%02x%02x\n' % (x,y,r,g,b))
+    cmd = 'PX %d %d %02x%02x%02x\n' % (x,y,r,g,b)
+    pxf.send(cmd.encode())
 
 #send info image
 img = Image.open('pxf_useage.bmp').convert('RGB')
 _,_,w,h = img.getbbox()  
-for x in range(w-1, 0):
-   for y in range(h-1, 0):
+for x in range(0, w-1):
+   for y in range(0, h-1):
      r,g,b = img.getpixel((x,y))
      send_pixel(x,y,r,g,b)
