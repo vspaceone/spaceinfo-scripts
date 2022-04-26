8b499a8d1ec1061da8e566fa9c16b47dc8c3518a
HACKER3000 <hacker3000@posteo.org>
2022-04-26 19:42:12 +0200

    fix stupid

--- space_status_monitor_switch/space_status_monitor_switch.py
+++ space_status_monitor_switch/space_status_monitor_switch.py
@@ -10,7 +10,7 @@ mqtt_server = "mqtt.int.vspace.one"
 mqtt_topic = "vspace/one/state/open"
 #config end
 
-client = Client(socket.gethostname()) #use hostname as client name
+client = mqtt.Client(socket.gethostname()) #use hostname as client name
 
 def handle_msg(client, userdata, message): 
     if (message.topic != mqtt_topic):
@@ -18,25 +18,26 @@ def handle_msg(client, userdata, message):
     msg_pl = str(message.payload.decode("utf-8")) #read msg
     msg_dc = json.loads(msg_pl) #decode msg
     
-    if (msg_dc['status'] !== "ok"):
+    if (msg_dc['status'] != "ok"):
         return
     
     #use two ifs with === to not trigger on None
-    if (msg_dc['data']['open'] === False): #turn monitor off when space closed
+    if (msg_dc['data']['open'] == False): #turn monitor off when space closed
         os.system("vcgencmd display_power 0")
-    else if (msg_dc['data']['open'] === True): #turn monitor on when space open
+    elif (msg_dc['data']['open'] == True): #turn monitor on when space open
         os.system("vcgencmd display_power 1")
-    else if (msg_dc['data']['open'] === None):
+    elif (msg_dc['data']['open'] == None):
         print("Space status message ok but no status set?!")
 
+def on_connect(client, userdata, flags, rc):
+    if rc != 0:
+         print("connect error")
+         exit(1)
+
 client.on_message=handle_msg
+client.on_connect=on_connect
 
- 
-all_ok = True
-
-all_ok &= client.connect(mqtt_server)
-all_ok &= client.subscribe(mqtt_topic,qos=2)
-all_ok &= client.loop_start()
-
-if (!all_ok):
-    quit 1
+client.connect(mqtt_server)
+client.subscribe(mqtt_topic,qos=2)
+client.loop_start()
+client.loop_forever()
